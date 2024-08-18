0d239bf872a92131293abd7c1565daf46ba1ac4f
Nageshwar <nageshpawarpatil@gmail.com>
2024-08-19 02:36:22 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-// const cookieParser = require("cookie-parser"); // currently not installed
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const router = require("./routes/index");
-const addCreatorController = require("./controllers/addCreator.controller");
-const getAllCreatorsController = require("./controllers/getAllCreators.controller");
-const getCurrentCreatorController = require("./controllers/getCurrentCreator.controller");
-const updateCreatorController = require("./controllers/updateCreator.controller");
 
 const app = express();
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://farminsta-technical-round-assessment-frontend.vercel.app",
+    origin:
+      process.env.FRONTEND_URL ||
+      "https://farminsta-technical-round-assessment-frontend.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json());
-// app.use(cookieParser()); // currently not installed
+app.use(cookieParser());
 
 // routes
 app.use("/api", router);
